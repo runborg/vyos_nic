37c3aab4b66c9c4843ef0cfc469303af25f17d6c
Runar Borge <runar@borge.nu>
2019-10-21 23:28:57 +0200

    cleaning up wireguard dump code

--- wireguard.py
+++ wireguard.py
@@ -16,55 +16,28 @@ def wireguard_dump():
             last_device = items[0]
             # We are currently entering a new node
             device, private_key, public_key, listen_port, fw_mark = items
-            if private_key == '(none)':
-                private_key = None
-            if public_key == '(none)':
-                public_key = None
-            listen_port = int(listen_port)
-            if fw_mark == 'off':
-                fw_mark = None
-            else:
-                fw_mark = int(fw_mark)
 
-            output[items[0]] = {
-                'private_key': private_key,
-                'public_key': public_key,
-                'listen_port': listen_port,
-                'fw_mark': fw_mark,
+            output[device] = {
+                'private_key': None if private_key == '(none)' else private_key,
+                'public_key': None if public_key == '(none)' else public_key,
+                'listen_port': int(listen_port),
+                'fw_mark': None if fw_mark == 'off' else int(fw_mark),
                 'peers': {},
                 } 
         else:
             # We are entering a peer
             device, public_key, preshared_key, endpoint, allowed_ips, latest_handshake, transfer_rx, transfer_tx, persistent_keepalive = items
-            if preshared_key == '(none)':
-                preshared_key = None
-            if endpoint == '(none)':
-                endpoint = None
-            if latest_handshake == '0':
-                latest_handshake = None
-            else:
-                latest_handshake = int(latest_handshake)
-            transfer_rx = int(transfer_rx)
-            transfer_tx = int(transfer_tx)
-            if persistent_keepalive == 'off':
-                persistent_keepalive = None
-            else:
-                persistent_keepalive = int(persistent_keepalive)
-            allowed_ips = allowed_ips.split(',')
             output[device][public_key] = {
-                'preshared_key': preshared_key,
-                'endpoint': endpoint,
-                'allowed_ips': allowed_ips,
-                'latest_handshake': latest_handshake,
-                'transfer_rx': transfer_rx,
-                'transfer_tx': transfer_tx,
-                'persistent_keepalive': persistent_keepalive,
+                'preshared_key': None if preshared_key == '(none)' else preshared_key,
+                'endpoint': None if endpoint == '(none)' else endpoint,
+                'allowed_ips': allowed_ips.split(','),
+                'latest_handshake': None if latest_handshake == '0' else int(latest_handshake),
+                'transfer_rx': int(transfer_rx),
+                'transfer_tx': int(transfer_tx),
+                'persistent_keepalive': None if persistent_keepalive == 'off' else int(persistent_keepalive),
            } 
-
     return output
             
-
-
 if __name__ == "__main__":
     import json
     print(json.dumps(wireguard_dump(), indent=4))
